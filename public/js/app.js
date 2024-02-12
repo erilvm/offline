aa0e727f8fdf261449d8d2d2d44bd4ee564e67e0
erilvm <erika.nemesio2002@gmail.com>
2024-02-12 17:47:55 -0600

    App-Shell

--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,3 @@
 if (navigator.serviceWorker) {
     navigator.serviceWorker.register('/sw.js')
 }
-if(window.caches) {
-
-    //console.log("Soporta cache")
-    //crear primer cache
-    caches.open('test-1')
-
-    caches.open('test-2')
-
-    //verificar si existe un cache
-    //caches.has('test-1').then(console.log)
-
-
-    //Eliminar cache
-    caches.delete('test-1').then(console.log)
-
-    //Guardar archivos en un cache=parametro que recicbe esa promesa
-
-    caches.open('cache-1').then(cache => {
-        cache.add('/index.html')
-        //agregar archivos
-
-        //agregar varios archivos
-        cache.addAll([
-            'logo192.png',
-            'logo512.png',
-            '/pages/offline.html'
-        ]).then(() => {
-            //Interceptar un archivo por otro
-            cache.put('index.html', new Response('Interceptado archivo index'))
-        })
-        
-        //Obtener todos los caches existen en la aplicacion y todos los metodos responden con una promesa
-        cache.keys().then()
-    })
-}
--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,30 @@
+//La creación del App Shell, se realizará dentro del proceso de instalación 
+self.addEventListener('install', event => {
 
-self.addEventListener('fetch', event => {
-    const offlineResponse = fetch('pages/offile.html')
+//Para eso, dentro del evento de instalación creamos una constante cacheAppShel, 
+// que va a almacenar la promesa que dispara el evento open para crear o abrir el caché 
+    const cacheAppShell = caches.open('cache-1').then(cache => {      
+//esta promesa retorna la creación del AppShell con el método addAll, que recibe un arreglo con 
+//cada uno de los archivos necesarios para el buen funcionamiento de la aplicación.
+        return cache.addAll([
+            '/',
+            '/index.html',
+            '/js/app.js',
+            '/sw.js',
+            'static/js/bundle.js',
+            'favicon.ico',
 
+        ])
 
-    const resp = fetch(event.request)
+    })
 
-    .catch(() => offlineResponse)
-    event.respondWith(resp)
+    event.waitUntil(cacheAppShell)
 })
+
+//El usuario verá en su aplicación únicamente los recursos que están almacenados en caché. 
+//Este proceso se hace dentro del evento fetch    
+self.addEventListener('fetch',event => {
+    //por medio de la propiedad  event.respondWith interceptando las solicitudes
+    event.responWith(caches.match(event.request))
+})
+
