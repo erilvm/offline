cb764276e1dca1f65dce1e0eb82c688917c95649
erilvm <erika.nemesio2002@gmail.com>
2024-02-11 20:21:52 -0600

    intro-cache

--- public/sw.js
+++ public/sw.js
@@ -8,3 +8,14 @@ self.addEventListener('fetch', event => {
     .catch(() => offlineResponse)
     event.respondWith(resp)
 })
+
+self.addEventListener("fetch", function (event) {
+    const offlineResponse = fetch("pages/offline.html")
+   
+    
+      const resp = fetch(event.request);
+      event.respondWith(resp.catch(() => offlineResponse));
+    
+      event.respondWith(resp);
+    });
+  
